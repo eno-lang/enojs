171c9d66dbed9fad13d16b53f6a971706652cb44
Simon Repp <simon@fdpl.io>
2018-05-23 07:58:50 +0200

    Assert correct reporter type on parser initialization

--- eno.js
+++ eno.js
@@ -4,6 +4,7 @@ const { EnoParseError, EnoValidationError } = require('./lib/errors.js');
 const messages = require('./lib/messages.js');
 
 const locales = Object.keys(messages);
+const reporters = ['html', 'terminal', 'text']
 
 // TODO: Parser and Builder (no generic dumper concept exists in eno)
 // TODO: - Pass default loaders to parse(..), which are always run when getting any values from the resulting document (e.g. ERB default loader to interpolate things)
@@ -32,10 +33,14 @@ const parse = (input, locale = 'en', reporter = 'text') => {
 
   if(!locales.includes(locale)) {
     throw new RangeError(
-      `The requested locale "${locale}" is not supported. Translation contributions are ` +
+      `The requested locale '${locale}' is not supported. Translation contributions are ` +
       'very welcome and an easy thing to do - only a few easy messages need ' +
       'to be translated!'
     );
+  }
+
+  if(!reporters.includes(reporter)) {
+    throw new RangeError(`The requested reporter '${reporter}' does not exist.`);
   }
 
   const parser = new EnoParser(input, messages[locale], reporter);
