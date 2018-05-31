6a9810f6a1f8bd29eef7defe1f3872a9385871af
Simon Repp <simon@fdpl.io>
2018-05-31 15:30:06 +0200

    Additions and tests for providing a message function to getError calls

--- specs/errors/validation/value_error.spec.js
+++ specs/errors/validation/value_error.spec.js
@@ -23,4 +23,94 @@ describe('validation.valueError', () => {
   test(`provides correct selection metadata`, () => {
     expect(error.selection).toEqual([[1, 10], [1, 14]]);
   });
+
+  describe('calling getError on a field', () => {
+    const document = eno.parse('language: yaml');
+
+    const field = document.sequential()[0];
+    const error = field.getError();
+
+    test(`provides correct message`, () => {
+      expect(error.message).toMatchSnapshot();
+    });
+
+    test(`provides correct selection metadata`, () => {
+      expect(error.selection).toEqual([[1, 10], [1, 14]]);
+    });
+
+    describe('providing a custom message', () => {
+      const document = eno.parse('language: yaml');
+
+      const field = document.sequential()[0];
+      const error = field.getError(' a highly custom error');
+
+      test(`provides correct message`, () => {
+        expect(error.message).toMatchSnapshot();
+      });
+
+      test(`provides correct selection metadata`, () => {
+        expect(error.selection).toEqual([[1, 10], [1, 14]]);
+      });
+    });
+
+    describe('providing an error function', () => {
+      const document = eno.parse('language: yaml');
+
+      const field = document.sequential()[0];
+      const error = field.getError(({ name, value }) => `${name} can not be ${value}`);
+
+      test(`provides correct message`, () => {
+        expect(error.message).toMatchSnapshot();
+      });
+
+      test(`provides correct selection metadata`, () => {
+        expect(error.selection).toEqual([[1, 10], [1, 14]]);
+      });
+    });
+  });
+
+  describe('calling getError on an empty element', () => {
+    const document = eno.parse('language:');
+
+    const empty = document.sequential()[0];
+    const error = empty.getError();
+
+    test(`provides correct message`, () => {
+      expect(error.message).toMatchSnapshot();
+    });
+
+    test(`provides correct selection metadata`, () => {
+      expect(error.selection).toEqual([[1, 9], [1, 9]]);
+    });
+
+    describe('providing a custom message', () => {
+      const document = eno.parse('language:');
+
+      const empty = document.sequential()[0];
+      const error = empty.getError('a highly custom error');
+
+      test(`provides correct message`, () => {
+        expect(error.message).toMatchSnapshot();
+      });
+
+      test(`provides correct selection metadata`, () => {
+        expect(error.selection).toEqual([[1, 9], [1, 9]]);
+      });
+    });
+
+    describe('providing an error function', () => {
+      const document = eno.parse('language:');
+
+      const empty = document.sequential()[0];
+      const error = empty.getError(({ name, value }) => `${name} can not be ${value}`);
+
+      test(`provides correct message`, () => {
+        expect(error.message).toMatchSnapshot();
+      });
+
+      test(`provides correct selection metadata`, () => {
+        expect(error.selection).toEqual([[1, 9], [1, 9]]);
+      });
+    });
+  });
 });
