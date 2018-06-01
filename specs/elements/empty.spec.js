236d3bd81f7449e3516a107a149c4dc5eb97cf9b
Simon Repp <simon@fdpl.io>
2018-06-01 09:20:25 +0200

    Rename empty element get accessor to value, add specs

---
+++ specs/elements/empty.spec.js
@@ -0,0 +1,67 @@
+const EnoEmpty = require('../../lib/elements/empty.js');
+
+const fabricate = () => {
+  const context = {};
+  const instruction = { name: 'language' };
+
+  return new EnoEmpty(context, instruction);
+}
+
+describe('EnoEmpty', () => {
+
+  test('is untouched after initialization', () => {
+    const empty = fabricate();
+    expect(empty.touched).toBe(false);
+  });
+
+  describe('raw()', () => {
+
+    test('returns the primitive object representation', () => {
+      const empty = fabricate();
+      expect(empty.raw()).toEqual({ language: null });
+    });
+
+  });
+
+  describe('toString()', () => {
+
+    test('returns a debug abstraction', () => {
+      const empty = fabricate();
+      expect(empty.toString()).toEqual('[Object EnoEmpty name="language"]');
+    });
+
+  });
+
+  describe('toStringTag symbol', () => {
+
+    test('returns a custom tag', () => {
+      const empty = fabricate();
+      expect(Object.prototype.toString.call(empty)).toEqual('[object EnoEmpty]');
+    });
+
+  });
+
+  describe('touch()', () => {
+
+    test('touches the element', () => {
+      const empty = fabricate();
+      empty.touch();
+      expect(empty.touched).toBe(true);
+    });
+
+  });
+
+  describe('value()', () => {
+
+    test('returns null', () => {
+      const empty = fabricate();
+      expect(empty.value()).toBe(null);
+    });
+
+    test('touches the element', () => {
+      const empty = fabricate();
+      const _ = empty.value();
+      expect(empty.touched).toBe(true);
+    });
+  });
+});
