be83678e3ad4b372dfbdfab7a4780e929fc69a2a
Simon Repp <simon@fdpl.io>
2018-06-04 10:26:40 +0200

    Account for empty values in EnoValue's toString method and extend specs

--- specs/elements/value.spec.js
+++ specs/elements/value.spec.js
@@ -1,44 +1,74 @@
 const EnoValue = require('../../lib/elements/value.js');
 
-const fabricate = () => {
-  const context = {};
-  const instruction = { name: 'language', value: 'eno' };
-
-  return new EnoValue(context, instruction);
-}
+const context = {};
+const instruction = { name: 'language', value: 'eno' };
+const instructionNamedEmpty = { name: 'language', value: null };
+const instructionUnnamedValue = { value: 'eno' };
+const instructionVoid = {};
+const parent = {};
 
 describe('EnoValue', () => {
 
+  let value;
+
+  beforeEach(() => {
+    value = new EnoValue(context, instruction, parent);
+  });
+
   test('is untouched after initialization', () => {
-    const enoValue = fabricate();
-    expect(enoValue.touched).toBe(false);
+    expect(value.touched).toBe(false);
+  });
+
+  describe('toString()', () => {
+    describe('with a name and a value', () => {
+      it('returns a debug abstraction', () => {
+        expect(value.toString()).toEqual('[Object EnoValue name="language" value="eno"]');
+      });
+    });
+
+    describe('with a name and no value', () => {
+      it('returns a debug abstraction', () => {
+        const namedEmptyValue = new EnoValue(context, instructionNamedEmpty, parent);
+        expect(namedEmptyValue.toString()).toEqual('[Object EnoValue name="language" value=null]');
+      });
+    });
+
+    describe('with an unnamed value', () => {
+      it('returns a debug abstraction', () => {
+        const unnamedValue = new EnoValue(context, instructionUnnamedValue, parent);
+        expect(unnamedValue.toString()).toEqual('[Object EnoValue value="eno"]');
+      });
+    });
+
+    describe('with no name and value', () => {
+      it('returns a debug abstraction', () => {
+        const voidValue = new EnoValue(context, instructionVoid, parent);
+        expect(voidValue.toString()).toEqual('[Object EnoValue value=null]');
+      });
+    });
   });
 
   describe('value()', () => {
 
     test('returns the value', () => {
-      const enoValue = fabricate();
-      expect(enoValue.value()).toEqual('eno');
+      expect(value.value()).toEqual('eno');
     });
 
     test('touches the element', () => {
-      const enoValue = fabricate();
-      const _ = enoValue.value();
-      expect(enoValue.touched).toBe(true);
+      const _ = value.value();
+      expect(value.touched).toBe(true);
     });
 
     describe('with loader function', () => {
 
       test('applies the loader', () => {
-        const enoValue = fabricate();
-        const result = enoValue.value(({ value }) => value.toUpperCase());
+        const result = value.value(({ value }) => value.toUpperCase());
         expect(result).toEqual('ENO');
       });
 
       test('touches the element', () => {
-        const enoValue = fabricate();
-        const _ = enoValue.value(({ value }) => value.toUpperCase());
-        expect(enoValue.touched).toBe(true);
+        const _ = value.value(({ value }) => value.toUpperCase());
+        expect(value.touched).toBe(true);
       });
 
     });
