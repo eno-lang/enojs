7057416d6de068d392d3d00b3ae21b124365e1af
Simon Repp <simon@fdpl.io>
2018-10-30 16:14:19 +0100

    Remove unused dependency in analyzer specs

--- specs/analyzer/analyze_helper.js
+++ specs/analyzer/analyze_helper.js
@@ -1,5 +1,4 @@
 const analyze = require('../../lib/parse_steps/analyze.js');
-const resolve = require('../../lib/parse_steps/resolve.js');
 const tokenize = require('../../lib/parse_steps/tokenize.js');
 
 module.exports = input => {
